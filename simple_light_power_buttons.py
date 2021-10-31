f8f52b2f34d5a792e7a4daad6edb1724568e681d
florianfelix <florianfelixmeyer@gmail.com>
2021-10-31 14:25:23 +0100

    init

---
+++ simple_light_power_buttons.py
@@ -0,0 +1,100 @@
+bl_info = {
+    "name": "Simple Light Power Buttons",
+    "author": "florianfelix",
+    "version": (0, 1),
+    "blender": (3, 0, 0),
+    "location": "Properties -> Top of Light Panel",
+    "description": "Double / Half Light Power Buttons",
+    "warning": "",
+    "doc_url": "",
+    "category": "Lighting",
+}
+
+
+import bpy
+from bpy.types import Operator
+from bpy.props import FloatProperty
+
+class OBJECT_OT_double_intensity(Operator):
+    """Double Light intensity"""
+    bl_idname = "object.double_intensity"
+    bl_label = "Power * 2"
+    bl_options = {'REGISTER', 'UNDO'}
+    bl_description = "Shift: *= 1.1, ctrl: *= 5"
+    
+    multiplier : FloatProperty(
+        name="multiplier",
+        description="multiplier",
+        default=2.0)
+
+    @classmethod
+    def poll(self, context):
+        if context.active_object.type == 'LIGHT':
+            return True
+        return False
+
+    def execute(self, context):
+        context.active_object.data.energy *= self.multiplier
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        if event.ctrl:
+            self.multiplier = 5
+        if event.shift:
+            self.multiplier = 1.1
+        self.execute(context)
+        return {'FINISHED'}
+
+class OBJECT_OT_half_intensity(Operator):
+    """Half Light intensity"""
+    bl_idname = "object.half_intensity"
+    bl_label = "Power / 2"
+    bl_options = {'REGISTER', 'UNDO'}
+    bl_description = "Shift: /= 1.1, ctrl: /= 5"
+
+    divider : FloatProperty(
+        name="multiplier",
+        description="multiplier",
+        default=2.0)
+        
+    @classmethod
+    def poll(self, context):
+        if context.active_object.type == 'LIGHT':
+            return True
+        return False
+
+    def execute(self, context):
+        context.active_object.data.energy /= self.divider
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        if event.ctrl:
+            self.divider = 5
+        if event.shift:
+            self.divider = 1.1
+        self.execute(context)
+        return {'FINISHED'}
+
+# Registration
+def intensity_buttons(self, context):
+    row = self.layout.row()
+    row.operator(
+        OBJECT_OT_double_intensity.bl_idname,
+        icon='PLUGIN')
+    row.operator(
+        OBJECT_OT_half_intensity.bl_idname,
+        icon='PLUGIN')
+
+def register():
+    bpy.utils.register_class(OBJECT_OT_double_intensity)
+    bpy.utils.register_class(OBJECT_OT_half_intensity)
+    bpy.types.CYCLES_LIGHT_PT_light.prepend(intensity_buttons)
+
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_double_intensity)
+    bpy.utils.unregister_class(OBJECT_OT_half_intensity)
+    bpy.types.CYCLES_LIGHT_PT_light.remove(intensity_buttons)
+
+if __name__ == "__main__":
+    register()
